8b73bb2fda8f13077e728c87eaf5bfcabfb96404
Lars De Pauw <depauwlars@gmail.com>
2015-06-25 12:36:34 +0200

    safe save

--- src/main/java/be/vdab/ApplicationConfiguration.java
+++ src/main/java/be/vdab/ApplicationConfiguration.java
@@ -1,5 +1,10 @@
+package be.vdab;
+
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("be.vdab")
 public class ApplicationConfiguration {
+
 }
